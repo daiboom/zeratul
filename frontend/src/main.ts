32b92f7f507ad76edb0d81607992d1042089e389
daiboom <dbchoi@aboutfishing.kr>
2023-04-23 17:05:50 +0900

    frontend ui setting

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,4 +4,19 @@ import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(vuetify);
+app.mount("#app");
